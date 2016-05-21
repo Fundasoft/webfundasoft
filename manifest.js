3f210713d3e5b40245431238bc0248917698efd9
Joaquin Rotharmel <diegojoaquinrotharmel@gmail.com>
2016-05-21 18:22:50 -0300

    Agregado de varias secciones, mejoras en la usabilidad, pequenias mejoras en disenio y responsive design

---
+++ manifest.js
@@ -0,0 +1,17 @@
+{
+	"name": "Fundasoft",
+	"short_name": "Fundasoft",
+	"start_url": "/",
+	"scope": "/",
+	"display": "standalone",
+	"theme_color": "#5C6BC0",
+	"background_color": "#EEE",
+	"fullscreen": true,
+	"icons":[
+		{
+			"src": "/icon.png",
+			"sizes": "512x512",
+			"type": "image/png"
+		}
+	]
+}
